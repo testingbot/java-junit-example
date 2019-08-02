b1cc0f72416811090afde3d16f443ba37f27f3b6
Jochen Delabie <jochen@Jochens-iMac.local>
2019-08-02 21:04:35 +0200

    add simple mobile test

---
+++ src/test/java/com/testingbot/MobileTest.java
@@ -0,0 +1,15 @@
+package com.testingbot;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class MobileTest extends TestingBotJUnitTest {
+
+  @Test
+  public void test() throws Exception {
+  	
+  }
+}
